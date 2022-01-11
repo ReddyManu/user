735c3cd5b62c0674c1c3ded408d600a14109f896
Manu Reddy <reddybmanohar@gmail.com>
2022-01-11 19:11:10 +0530

    Roboshop user & shipping main

--- Terraform-mutable/main.tf
+++ Terraform-mutable/main.tf
@@ -1,13 +1,13 @@
 module "user" {
-  source = "git::https://github.com/ReddyManu/Terraform-mutable.git//app-module"
-  INSTANCE_TYPE = var.INSTANCE_TYPE
-  ENV = var.ENV
-  OD_INSTANCE_COUNT = var.OD_INSTANCE_COUNT
+  source              = "git::https://github.com/ReddyManu/Terraform-mutable.git//app-module"
+  INSTANCE_TYPE       = var.INSTANCE_TYPE
+  ENV                 = var.ENV
+  OD_INSTANCE_COUNT   = var.OD_INSTANCE_COUNT
   SPOT_INSTANCE_COUNT = var.SPOT_INSTANCE_COUNT
-  COMPONENT = "user"
-  PORT = 8080
-  IS_PRIVATE_LB = true
-  LB_RULE_PRIORITY = 105
+  COMPONENT           = "user"
+  PORT                = 8080
+  IS_PRIVATE_LB       = true
+  LB_RULE_PRIORITY    = 105
 }
 
 output "INSTANCE_IDS" {
